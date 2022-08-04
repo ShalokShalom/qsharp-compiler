01019149eab759cd80d8af06b31bb1533b637843
Andres Paz <anpaz@microsoft.com>
2022-08-04 00:07:26 -0700

    Make unsupported capabilities a warning.

--- src/QsCompiler/DataStructures/Diagnostics.fs
+++ src/QsCompiler/DataStructures/Diagnostics.fs
@@ -171,8 +171,6 @@ type ErrorCode =
     | ResultComparisonNotInOperationIf = 5024
     | ReturnInResultConditionedBlock = 5025
     | SetInResultConditionedBlock = 5026
-    | UnsupportedCallableCapability = 5027
-    | UnsupportedClassicalCapability = 5028
 
     | CallableRedefinition = 6001
     | CallableOverlapWithTypeConstructor = 6002
@@ -334,6 +332,7 @@ type WarningCode =
     | ReturnInResultConditionedBlock = 5025
     | SetInResultConditionedBlock = 5026
     | UnsupportedCallableCapability = 5027
+    | UnsupportedClassicalCapability = 5028
 
     | NamespaceAleadyOpen = 6003
     | NamespaceAliasIsAlreadyDefined = 6004
@@ -650,14 +649,14 @@ type DiagnosticItem =
             | ErrorCode.SetInResultConditionedBlock ->
                 "The variable \"{0}\" cannot be reassigned here. "
                 + "In conditional blocks that depend on a measurement result, the target {1} only supports reassigning variables that were declared within the block."
-            | ErrorCode.UnsupportedCallableCapability ->
-                "The callable {0} requires runtime capabilities which are not supported by the target {1}."
-                + Environment.NewLine
-                + "Result Opacity: {2}"
-                + Environment.NewLine
-                + "Classical Capability: {3}"
-            | ErrorCode.UnsupportedClassicalCapability ->
-                "This construct requires a classical runtime capability that is not supported by the target {0}: {1}."
+            // | ErrorCode.UnsupportedCallableCapability ->
+            //     "The callable {0} requires runtime capabilities which are not supported by the target {1}."
+            //     + Environment.NewLine
+            //     + "Result Opacity: {2}"
+            //     + Environment.NewLine
+            //     + "Classical Capability: {3}"
+            // | ErrorCode.UnsupportedClassicalCapability ->
+            //     "This construct requires a classical runtime capability that is not supported by the target {0}: {1}."
 
             | ErrorCode.CallableRedefinition ->
                 "Invalid callable declaration. A function or operation with the name \"{0}\" already exists."
@@ -915,7 +914,7 @@ type DiagnosticItem =
                 + " [{1}: ln {2}, cn {3}]"
             | WarningCode.UnsupportedCallableCapability ->
                 "{0}: "
-                + DiagnosticItem.Message(ErrorCode.UnsupportedCallableCapability, args |> Seq.skip 4)
+                // + DiagnosticItem.Message(ErrorCode.UnsupportedCallableCapability, args |> Seq.skip 4)
                 + " [{1}: ln {2}, cn {3}]"
 
             | WarningCode.NamespaceAleadyOpen -> "The namespace is already open."
